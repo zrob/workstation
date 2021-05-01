ba3bb33bc42d997061313ab2d27ff7b30f5c1ca1
Zach Robinson <zach.robinson@gmail.com>
2021-04-30 17:06:01 -0700

    remove homebrew install. it is a pre-req

--- install.sh
+++ install.sh
@@ -7,13 +7,7 @@ set -o xtrace
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Install Homebrew and install all the homebrew things
-
-if ! command -v brew &> /dev/null
-then
-    echo "'brew' could not be found. Installing now..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
+# Install Homebrew stuff
 
 brew bundle install
 
