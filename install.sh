d99019ee62d6ba64a2ab16de855510a23aadd9f5
Zach Robinson <zach.robinson@gmail.com>
2021-05-06 09:39:34 -0700

    cleanup install script

--- install.sh
+++ install.sh
@@ -3,94 +3,107 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-#set -o xtrace
+# set -o xtrace
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# Install Homebrew stuff
+brew_bundle() {
+    brew bundle install
+}
 
-brew bundle install
+setup_oh_my_zsh() {
+    if [ ! -d "${HOME}/.oh-my-zsh" ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
+    else
+        "${ZSH}/tools/upgrade.sh"
+    fi
 
-# Install and setup oh my zsh
+    custom_dir=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-if [ ! -d "${HOME}/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
-else
-    "${ZSH}/tools/upgrade.sh"
-fi
+    if [ ! -d "${custom_dir}/themes/powerlevel10k" ]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+            "${custom_dir}/themes/powerlevel10k"
+    else
+        pushd "${custom_dir}/themes/powerlevel10k"
+            git pull
+        popd
+    fi
 
-custom_dir=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+    if [ ! -d "${custom_dir}/plugins/zsh-syntax-highlighting" ]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+            "${custom_dir}/plugins/zsh-syntax-highlighting"
+    else
+        pushd "${custom_dir}/plugins/zsh-syntax-highlighting"
+            git pull
+        popd
+    fi
 
-if [ ! -d "${custom_dir}/themes/powerlevel10k" ]; then
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
-        "${custom_dir}/themes/powerlevel10k"
-else
-    pushd "${custom_dir}/themes/powerlevel10k"
-        git pull
-    popd
-fi
+    if [ ! -d "${custom_dir}/plugins/zsh-autosuggestions" ]; then
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+            "${custom_dir}/plugins/zsh-autosuggestions"
+    else
+        pushd "${custom_dir}/plugins/zsh-autosuggestions"
+            git pull
+        popd
+    fi
+}
 
-if [ ! -d "${custom_dir}/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-        "${custom_dir}/plugins/zsh-syntax-highlighting"
-else
-    pushd "${custom_dir}/plugins/zsh-syntax-highlighting"
-        git pull
-    popd
-fi
+setup_golang() {
+    GOPATH="${HOME}/workspace/go"
+    mkdir -p "${GOPATH}/src"
+    mkdir -p "${GOPATH}/bin"
+}
 
-if [ ! -d "${custom_dir}/plugins/zsh-autosuggestions" ]; then
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git \
-        "${custom_dir}/plugins/zsh-autosuggestions"
-else
-    pushd "${custom_dir}/plugins/zsh-autosuggestions"
-        git pull
-    popd
-fi
+setup_dotfiles() {
+    cp -f "${__dir}/dotFiles/.gitconfig" ~/.gitconfig
+    cp -f "${__dir}/dotFiles/.zshrc" ~/.zshrc
+    cp -f "${__dir}/dotFiles/.p10k.zsh" ~/.p10k.zsh
+    cp -f "${__dir}/dotFiles/.nanorc" ~/.nanorc
+}
 
-# Setup golang
+setup_spectacle() {
+    local desired_prefs="${__dir}/assets/spectacle/Shortcuts.json"
+    local current_prefs="${HOME}/Library/Application Support/Spectacle/Shortcuts.json"
 
-GOPATH="${HOME}/workspace/go"
-mkdir -p "${GOPATH}/src"
-mkdir -p "${GOPATH}/bin"
+    if ! diff "${desired_prefs}" "${current_prefs}" >/dev/null 2>&1; then
+        cp -f "${desired_prefs}" "${current_prefs}"
+    fi
+}
 
-# Copy dotfiles
+setup_personal_hooks() {
+    mkdir -p "${HOME}/bin.personal"
 
-cp -f "${__dir}/dotFiles/.gitconfig" ~/.gitconfig
-cp -f "${__dir}/dotFiles/.zshrc" ~/.zshrc
-cp -f "${__dir}/dotFiles/.p10k.zsh" ~/.p10k.zsh
-cp -f "${__dir}/dotFiles/.nanorc" ~/.nanorc
+    if [ ! -f "${HOME}/.config.personal" ]; then
+        cp "${__dir}/assets/config.personal-template" ~/.config.personal
+    fi
+}
 
-# Setup spectacle
+print_outro() {
+cat << EOF
 
-desired_prefs="${__dir}/assets/spectacle/Shortcuts.json"
-current_prefs="${HOME}/Library/Application Support/Spectacle/Shortcuts.json"
++=================================+
+Great job! You win some extra setup
++=================================+
 
-if ! diff "${desired_prefs}" "${current_prefs}" >/dev/null 2>&1; then
-	cp -f "${desired_prefs}" "${current_prefs}"
-fi
+Configure iTerm (Preferences > General > Preferences)
+${__dir}/assets/iterm
 
-# Setup personal config and bin
+Install Powerlevel10k fonts
+p10k configure
 
-mkdir -p "${HOME}/bin.personal"
-[ ! -f "${HOME}/.config.personal" ] && \
-    cp "${__dir}/assets/config.personal-template" ~/.config.personal
+Reload Terminal:
+source ~/.zshrc"
+EOF
+}
 
-# Manual followup
+main() {
+    brew_bundle
+    setup_oh_my_zsh
+    setup_golang
+    setup_dotfiles
+    setup_spectacle
+    setup_personal_hooks
+    print_outro
+}
 
-echo
-echo "+==========================+"
-echo "Great job! Here's some extra setup"
-echo "+==========================+"
-echo
-
-echo "Configure iTerm (Preferences > General > Preferences)"
-echo "${__dir}/assets/iterm"
-echo
-
-echo "Install Powerlevel10k fonts"
-echo "p10k configure"
-echo
-
-echo "Reload Terminal:"
-echo "source ~/.zshrc"
+main
