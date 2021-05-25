1897c0501bd93c60468548611c782280cc0bdb9b
Zach Robinson <zach.robinson@gmail.com>
2021-05-24 20:50:58 -0700

    add sudo and cleanup

--- aptfile.sh
+++ aptfile.sh
@@ -3,7 +3,7 @@
 # Homebrew doesn't work on linux arm.
 # Trying out using this to mirror brew file for linux arm.
 
-sudo apt-get update && apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
     direnv \
     fzf \
     git \
@@ -15,7 +15,8 @@ sudo apt-get update && apt-get install -y \
     tree \
     watch \
     wget \
-    zsh-syntax-highlighting
+    zsh-syntax-highlighting \
+    && sudo rm -rf /var/lib/apt/lists/*
 
 # No apt analogs, just skipping
     # kind
