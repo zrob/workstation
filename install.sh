490a9a2abc9fb1ce2ea48a6a1fdf5942d342207d
Zach Robinson <zach.robinson@gmail.com>
2022-02-18 19:47:36 -0800

    inline krew install and use krew binary directly

--- install.sh
+++ install.sh
@@ -307,11 +307,24 @@ setup_personal_hooks() {
 }
 
 setup_krew() {
-    "${__dir}/install-krew.sh"
-    kubectl krew update
-    kubectl krew install tree
-    kubectl krew install lineage
-    kubectl krew upgrade
+    local os arch krew
+
+    os="$(uname | tr '[:upper:]' '[:lower:]')"
+    arch="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
+    krew="krew-${os}_${arch}"
+
+    KREW_TMPDIR="$(mktemp -d)"
+    trap '{ rm -rf -- "${KREW_TMPDIR}"; }' EXIT
+
+    cd "${KREW_TMPDIR}"
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${krew}.tar.gz"
+    tar zxvf "${krew}.tar.gz"
+
+    ./"${krew}" install krew
+    ./"${krew}" update
+    ./"${krew}" install tree
+    ./"${krew}" install lineage
+    ./"${krew}" upgrade
 }
 
 print_outro() {
