2703e04a9749b9fc150d65e7c0eba7ab19c0f360
Zach Robinson <zach.robinson@gmail.com>
2022-02-18 19:57:14 -0800

    add kubectl

--- aptfile.sh
+++ aptfile.sh
@@ -3,20 +3,26 @@
 # Homebrew doesn't work on linux arm.
 # Trying out using this to mirror brew file for linux arm.
 
+# add google cloud signing key
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# add kubernetes repo
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
 sudo apt-get update && sudo apt-get install -y \
     direnv \
     fzf \
     git \
     jq \
+    kubectl \
     ncdu \
-    socat \
+    python3 \
     silversearcher-ag \
+    socat \
     thefuck \
     tree \
     watch \
     wget \
-    zsh-syntax-highlighting \
-    python3
+    zsh-syntax-highlighting
 
 # No apt analogs, just skipping
     # kind
