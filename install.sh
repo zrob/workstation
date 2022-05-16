31d70ecade6b91cf3350fd9a1e7556848e719ee4
Zach Robinson <zach.robinson@gmail.com>
2022-05-15 21:52:32 -0700

    use quad9

--- install.sh
+++ install.sh
@@ -335,7 +335,11 @@ setup_dns() {
     if ! profiles list | grep -q com.zach.cloudflare.doh; then
         open "${__dir}/assets/osx-dns-profile/cloudflare-doh.mobileconfig"
     fi
-    networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001
+     if ! profiles list | grep -q com.zach.quad9.doh; then
+        open "${__dir}/assets/osx-dns-profile/quad9-doh.mobileconfig"
+    fi
+    # networksetup -setdnsservers Wi-Fi 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001
+    networksetup -setdnsservers Wi-Fi 9.9.9.9 149.112.112.112 2620:fe::fe 2620:fe::9
 }
 
 print_outro() {
