d286ba3f3d5d0b8dc39c35efedeb5f0aaad2a84a
Zach Robinson <zach.robinson@gmail.com>
2024-06-26 14:43:37 -0700

    update work profile

--- profiles/work.zsh
+++ profiles/work.zsh
@@ -1,1 +1,2 @@
-export HOMEBREW_BUNDLE_CASK_SKIP="${HOMEBREW_BUNDLE_CASK_SKIP:-} dropbox keepassxc"
+export HOMEBREW_BUNDLE_CASK_SKIP="${HOMEBREW_BUNDLE_CASK_SKIP:-} dropbox keepassxc zoom google-chrome"
+export WORKSTATION_SKIP="${WORKSTATION_SKIP:-} dns"
