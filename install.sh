394d315f28d32aef1d0d78b4a537df8764538c6d
Zach Robinson <zach.robinson@gmail.com>
2025-02-06 20:49:08 -0800

    temp commit, view ruby logs

--- install.sh
+++ install.sh
@@ -248,13 +248,13 @@ setup_ruby() {
     ruby-install --latest #> "$logfile" 2>&1
     ruby-install --cleanup --no-reinstall ruby #>> "$logfile" 2>&1
 
-    if ! grep "Ruby is already installed" "$logfile" >/dev/null; then
-        installed_version=$(grep "Successfully installed ruby" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
-        echo "$installed_version" > "${HOME}/.ruby-version"
-    else
-        installed_version=$(grep "Ruby is already installed" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
-        echo "$installed_version" > "${HOME}/.ruby-version"
-    fi
+    # if ! grep "Ruby is already installed" "$logfile" >/dev/null; then
+    #     installed_version=$(grep "Successfully installed ruby" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
+    #     echo "$installed_version" > "${HOME}/.ruby-version"
+    # else
+    #     installed_version=$(grep "Ruby is already installed" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
+    #     echo "$installed_version" > "${HOME}/.ruby-version"
+    # fi
 
     # chruby doesn't play nice with nounset
     set +o nounset
