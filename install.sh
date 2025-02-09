9222a5fd074e830a29b9453ff0acfc34102730d4
Zach Robinson <zach.robinson@gmail.com>
2025-02-08 16:13:45 -0800

    revert temp commits

--- install.sh
+++ install.sh
@@ -245,31 +245,31 @@ setup_ruby() {
 
     # install latest ruby
     echo "Installing ruby. Logs are in '${logfile}'"
-    ruby-install --latest #> "$logfile" 2>&1
-    ruby-install --cleanup --no-reinstall ruby #>> "$logfile" 2>&1
+    ruby-install --latest > "$logfile" 2>&1
+    ruby-install --cleanup --no-reinstall ruby >> "$logfile" 2>&1
 
-    # if ! grep "Ruby is already installed" "$logfile" >/dev/null; then
-    #     installed_version=$(grep "Successfully installed ruby" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
-    #     echo "$installed_version" > "${HOME}/.ruby-version"
-    # else
-    #     installed_version=$(grep "Ruby is already installed" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
-    #     echo "$installed_version" > "${HOME}/.ruby-version"
-    # fi
+    if ! grep "Ruby is already installed" "$logfile" >/dev/null; then
+        installed_version=$(grep "Successfully installed ruby" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
+        echo "$installed_version" > "${HOME}/.ruby-version"
+    else
+        installed_version=$(grep "Ruby is already installed" "$logfile" | awk '{n=split($0,A,"\/"); print A[n]}')
+        echo "$installed_version" > "${HOME}/.ruby-version"
+    fi
 
     # chruby doesn't play nice with nounset
-    # set +o nounset
-    # if command -v brew >/dev/null; then
-    #     source "$(brew --prefix)/opt/chruby/share/chruby/chruby.sh"
-    # else
-    #     source /usr/local/share/chruby/chruby.sh
-    # fi
-    # chruby "$(cat "${HOME}/.ruby-version")"
-    # chruby
-    # set -o nounset
+    set +o nounset
+    if command -v brew >/dev/null; then
+        source "$(brew --prefix)/opt/chruby/share/chruby/chruby.sh"
+    else
+        source /usr/local/share/chruby/chruby.sh
+    fi
+    chruby "$(cat "${HOME}/.ruby-version")"
+    chruby
+    set -o nounset
 
-    # gem install bundler --no-document
+    gem install bundler --no-document
 
-    # tail -n 1 "$logfile"
+    tail -n 1 "$logfile"
 }
 
 setup_dotfiles() {
